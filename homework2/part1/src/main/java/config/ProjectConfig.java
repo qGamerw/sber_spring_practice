cd27a949865458be67509445f0c3876937fdd42a
faleot724 <faleot724@gmail.com>
2023-06-20 21:24:57 +0300

    update

--- homework2/part1/src/main/java/config/ProjectConfig.java
+++ homework2/part1/src/main/java/config/ProjectConfig.java
@@ -12,32 +12,32 @@ public class ProjectConfig {
 
     @Bean
     public Person person() {
-        var person = new Person(parrot1(), parrot2(), dog(), cat());
+        var person = new Person(getParrot1(), getParrot2(), getDog(), getCat());
         return person;
     }
     @Bean
-    public Parrot parrot1() {
+    public Parrot getParrot1() {
         var p = new Parrot();
         p.setName("Koko");
         return p;
     }
 
     @Bean
-    public Parrot parrot2() {
+    public Parrot getParrot2() {
         var parrot = new Parrot();
         parrot.setName("Kiko");
         return parrot;
     }
 
     @Bean
-    public Dog dog() {
+    public Dog getDog() {
         var dog = new Dog();
         dog.setName("Bobi");
         return dog;
     }
 
     @Bean
-    public Cat cat() {
+    public Cat getCat() {
         var cat = new Cat();
         cat.setName("Murky");
         return cat;
